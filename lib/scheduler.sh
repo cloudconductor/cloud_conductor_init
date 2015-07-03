28cd2d3b54a8a5f059c663620bdeb29ff38ebdec
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-07-03 17:22:03 +0900

    Add proxy-host/port configuration from environment variable

--- lib/scheduler.sh
+++ lib/scheduler.sh
@@ -13,10 +13,16 @@ install_scheduler() {
 
   directory ${scheduler_config_dir} root:root 755 || return $?
 
+  values=(`echo $http_proxy | tr -s '/:' ' '`)
+  proxy_host=${values[1]}
+  proxy_port=${values[2]}
+
   file_copy ${tmpls_dir}/default/config.yml ${scheduler_config_dir}/config.yml root:root 644 || return $?
   sed -i \
       -e "s@__role__@${ROLE}@g" \
       -e "s@__token__@${CONSUL_SECRET_KEY}@g" \
+      -e "s@__proxy-host__@${proxy_host}@g" \
+      -e "s@__proxy-port__@${proxy_port}@g" \
       ${scheduler_config_dir}/config.yml \
       || return $?
 
