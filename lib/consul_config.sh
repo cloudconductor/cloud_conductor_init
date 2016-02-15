29a09c473b424c14acee8ddd0eda93d6d5457c26
Yoshimi Tominaga <tominaga.yoshimi@tis.co.jp>
2016-02-15 10:22:59 +0900

    Modify consul download URL

--- lib/consul_config.sh
+++ lib/consul_config.sh
@@ -4,17 +4,17 @@ consul_ssl_cert='/etc/pki/tls/certs/consul.crt'
 consul_ssl_key='/etc/pki/tls/private/consul.key'
 
 consul_install_dir='/usr/local/bin'
-consul_version='0.5.2_linux_amd64'
+consul_version='0.5.2'
 consul_data_dir='/var/lib/consul'
 consul_config_dir='/etc/consul.d'
 etc_config_dir='/etc/sysconfig/consul'
 
 install_consul() {
-  remote_file https://dl.bintray.com/mitchellh/consul/${consul_version}.zip \
-      ${tmp_dir}/${consul_version}.zip \
+  remote_file https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip \
+      ${tmp_dir}/consul_${consul_version}_linux_amd64.zip \
       || return $?
 
-  unzip -o -d ${consul_install_dir} ${tmp_dir}/${consul_version}.zip || return $?
+  unzip -o -d ${consul_install_dir} ${tmp_dir}/consul_${consul_version}_linux_amd64.zip || return $?
   chmod 755 ${consul_install_dir}/consul || return $?
 
 
